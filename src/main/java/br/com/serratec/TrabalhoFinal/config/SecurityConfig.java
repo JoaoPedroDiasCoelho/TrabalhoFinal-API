1c5eba27ab600727c09f4a2adf81dc350197682e
JoaoPedroDiasCoelho <diascoelho.jpgamer@gmail.com>
2025-10-24 20:59:16 -0300

    Fix: securityConfig e vendas_db

--- src/main/java/br/com/serratec/TrabalhoFinal/config/SecurityConfig.java
+++ src/main/java/br/com/serratec/TrabalhoFinal/config/SecurityConfig.java
@@ -1,26 +1,26 @@
-package br.com.serratec.TrabalhoFinal.config;
+// package br.com.serratec.TrabalhoFinal.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
+// @Configuration
+// @EnableWebSecurity
+// public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers(AntPathRequestMatcher.antMatcher("/**")).permitAll()
-                .requestMatchers(AntPathRequestMatcher.antMatcher("/swagger-ui/**")).permitAll()
-                .requestMatchers(AntPathRequestMatcher.antMatcher("/v3/api-docs/**")).permitAll()
-                .anyRequest().permitAll()
-            );
+//     @Bean
+//     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//         http.csrf(csrf -> csrf.disable())
+//             .authorizeHttpRequests(authorize -> authorize
+//                 .requestMatchers(AntPathRequestMatcher.antMatcher("/**")).permitAll()
+//                 .requestMatchers(AntPathRequestMatcher.antMatcher("/swagger-ui/**")).permitAll()
+//                 .requestMatchers(AntPathRequestMatcher.antMatcher("/v3/api-docs/**")).permitAll()
+//                 .anyRequest().permitAll()
+//             );
 
-        return http.build();
-    }
-}
+//         return http.build();
+//     }
+// }

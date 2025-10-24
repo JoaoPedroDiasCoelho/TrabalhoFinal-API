d6d55942815eb5d815c085baa1129b456ed21210
MatheusSchwhartz <matheusschwhartz@gmail.com>
2025-10-23 22:35:18 -0300

    fix: Atualizaçao Getters and Setters e Constructor

--- src/main/java/br/com/serratec/TrabalhoFinal/dto/CategoriaDTO.java
+++ src/main/java/br/com/serratec/TrabalhoFinal/dto/CategoriaDTO.java
@@ -2,17 +2,35 @@ package br.com.serratec.TrabalhoFinal.dto;
 
 
 import br.com.serratec.TrabalhoFinal.entity.Categoria;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
+
 @NoArgsConstructor
 public class CategoriaDTO {
     private Long id;
     private String nome;
+    
+    
+	public CategoriaDTO(Categoria entity) {
+		super();
+		this.id = entity.getId();
+		this.nome = entity.getNome();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    public CategoriaDTO(Categoria entity) {
-        this.id = entity.getId();
-        this.nome = entity.getNome();
-    }
+    
+    
+    
 }
